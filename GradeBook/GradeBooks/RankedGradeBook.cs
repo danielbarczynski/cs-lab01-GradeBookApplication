27a2b9bb053fbbd3a9da6ef9349a2714a347f38a
Daniel <daniel.barczynski@onet.pl>
2022-03-29 22:04:30 +0200

    changed classes from internal to public

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
